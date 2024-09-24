d950e493c2b00b16bac566af8aa6cd995dd98f5a
Peter Danenberg <pcd@roxygen.org>
2024-09-23 23:22:47 -0700

    Add license

--- history/slides/components/AnimatableSvg.vue
+++ history/slides/components/AnimatableSvg.vue
@@ -1,3 +1,19 @@
+<!--
+ Copyright 2024 -l
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+-->
+
 <template>
   <!-- Removed 'h-auto' since 'fixed-height' should define the height -->
   <div :id="containerId" class="svg-container w-full"></div>
